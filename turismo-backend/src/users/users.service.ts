729fba4270ebff869f1e601dbb1377c06aac3e65
kevinseya <kevinseya@gmail.com>
2026-01-29 23:47:57 -0500

    feat: :sparkles: agregar lógica para verificar si un usuario está siguiendo a otro en el método findOne

--- turismo-backend/src/users/users.service.ts
+++ turismo-backend/src/users/users.service.ts
@@ -4,7 +4,32 @@ import { $Enums } from '@prisma/client';
 
 @Injectable()
 export class UsersService {
+		async isFollowing(followerId: number, followingId: number): Promise<boolean> {
+			const follow = await this.prisma.follow.findUnique({
+				where: {
+					followerId_followingId: {
+						followerId,
+						followingId,
+					},
+				},
+			});
+			return !!follow;
+		}
 	constructor(private prisma: PrismaService) {}
+
+	findOne(id: number) {
+		return this.prisma.user.findUnique({
+			where: { id },
+			select: {
+				id: true,
+				name: true,
+				email: true,
+				role: true,
+				isActive: true,
+				createdAt: true,
+			},
+		});
+	}
 
 	findAll() {
 		return this.prisma.user.findMany({
